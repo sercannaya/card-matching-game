55e305d83fd8cb0371a47806a89eef90896df375
Sercan NAYA <sercannayaa@gmail.com>
2023-01-12 20:18:17 +0300

    feat: card has front and back and can be flipped

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@
     <CardComponents 
       v-for="(card, index) in cardList" 
       :key="`card-${index}`" 
-      :value="card"
+      :value="card.value"
+      :visible="card.visible"
+      :position="card.position"
+      @select-card="flipCard"
     />
   </section>
 </template>
 
 <script>
+import { ref } from 'vue'
 import CardComponents from './components/CardComponents.vue';
 
 export default {
@@ -18,14 +22,24 @@ export default {
     CardComponents
   },
   setup() {
-    const cardList = []
+    const cardList = ref([])
 
     for (let i = 0; i < 16; i++) {
-      cardList.push(i)
+      cardList.value.push({
+        value: i,
+        visible: false,
+        position: i
+      }
+      )
+    }
+
+    const flipCard = (payload) => {
+      cardList.value[payload.position].visible = true
     }
 
     return {
-      cardList
+      cardList,
+      flipCard
     }
   }
 }
@@ -39,10 +53,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.card {
-  border: 5px solid #ccc;
 }
 
 .game-board {
